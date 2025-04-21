0d99ee6980380daf6b491ab85adb090a0fd4f5ff
Gagun45 <selyanchyn45@gmail.com>
2025-04-21 21:56:37 +0300

    default locale, services page translated

--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -1,23 +1,24 @@
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
-const SERVICES = [
-  "Діагностика пристроїв",
-  "Заміна дисплеїв та сенсорів",
-  "Ремонт плат (пайка, відновлення після води)",
-  "Очищення та обслуговування ПК/ноутбуків",
-  "Перепрошивка та розблокування пристроїв",
-  "Заміна батарей",
-  "Встановлення програмного забезпечення",
-  "Ремонт ігрових консолей (PS5, Xbox, Nintendo)",
-];
-
 export default function ServicesPage() {
+  const t = useTranslations("ServicesPage");
+  const SERVICES = [
+    t("1st"),
+    t("2nd"),
+    t("3rd"),
+    t("4th"),
+    t("5th"),
+    t("6th"),
+    t("7th"),
+    t("8th"),
+  ];
   return (
     <main className="flex">
       <section className="flex flex-col w-full gap-6 sm:gap-12 xl:gap-9 2xl:gap-12 lg:w-2/3">
-        <h1 className="text-center text-4xl md:text-6xl">Наші послуги</h1>
+        <h1 className="text-center text-4xl md:text-6xl">{t("title")}</h1>
         <div className="flex flex-col gap-4">
           {SERVICES.map((service) => (
             <div key={service} className="flex items-center gap-4">
